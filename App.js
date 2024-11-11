7753d19b70626716f401d6c499696b54c9caa570
Nicholas <quackshows11@gmail.com>
2024-11-11 10:02:23 +0100

    Used prettier

--- App.js
+++ App.js
@@ -1,72 +1,79 @@
-import React, {useState} from 'react'
-import { Platform, StyleSheet, Text, View, TextInput, KeyboardAvoidingView, TouchableOpacity, ImageBackground, Keyboard, FlatList } from 'react-native';
+import React, { useState } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  ImageBackground,
+  Keyboard,
+  FlatList,
+} from "react-native";
 import Task from "./components/Task";
 
 export default function App() {
-
   const [task, setTask] = useState("");
   const [taskItems, setTaskItems] = useState([]);
 
   const date = new Date().toLocaleDateString("en-GB", {
     year: "numeric",
     month: "long",
-    day:"numeric"
-  })
+    day: "numeric",
+  });
 
   const handleAddTask = () => {
     setTaskItems([...taskItems, task]);
     setTask("");
     Keyboard.dismiss(); // This keyboard seems to be a little buggy
-    };
+  };
   const completeTask = (index) => {
     let itemsCopy = [...taskItems];
     itemsCopy.splice(index, 1);
     setTaskItems(itemsCopy);
   };
 
-  const renderTaskItem = ({item, index}) => (
-    <Task
-      text={item}
-      index={index}
-      completeTask={completeTask}
-    />
+  const renderTaskItem = ({ item, index }) => (
+    <Task text={item} index={index} completeTask={completeTask} />
   );
 
   return (
     <ImageBackground
-      source={require('./assets/todo-background.jpg')}
+      source={require("./assets/todo-background.jpg")}
       style={styles.background}
       resizeMode="cover"
     >
       <View style={styles.container}>
-
         <View style={styles.taskWrapper}>
           <Text style={styles.sectionTitle}>Today's Task</Text>
-          <Text style={styles.dateTitle}>{date}
-        </Text>
+          <Text style={styles.dateTitle}>{date}</Text>
 
-        <View style={styles.listBackground}>
-          <FlatList
-          data={taskItems}
-          renderItem={renderTaskItem}
-          keyExtractor={(item, index) => index.toString()}
-          contentContainerStyle={styles.items}
-          style={{ }}
-          />
-        </View>
-
-        
+          <View style={styles.listBackground}>
+            <FlatList
+              data={taskItems}
+              renderItem={renderTaskItem}
+              keyExtractor={(item, index) => index.toString()}
+              contentContainerStyle={styles.items}
+              style={{}}
+            />
+          </View>
         </View>
 
         <KeyboardAvoidingView
-        behavior={Platform.OS == "ios" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+          style={styles.writeTaskWrapper}
         >
-          <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)}/>
+          <TextInput
+            style={styles.input}
+            placeholder={"Write a task"}
+            value={task}
+            onChangeText={(text) => setTask(text)}
+          />
 
-          <TouchableOpacity onPress={() => handleAddTask()} >
+          <TouchableOpacity onPress={() => handleAddTask()}>
             <View style={styles.addWrapper}>
-            <Text style={styles.addText}> + </Text>
+              <Text style={styles.addText}> + </Text>
             </View>
           </TouchableOpacity>
         </KeyboardAvoidingView>
@@ -91,16 +98,16 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: "light",
     paddingTop: 2,
-    paddingLeft:2,
+    paddingLeft: 2,
     paddingBottom: 30,
   },
   items: {
-    shadowColor:"black",
-    shadowRadius:10,
+    shadowColor: "black",
+    shadowRadius: 10,
   },
   listBackground: {
-    height: '75%', 
-    backgroundColor:"rgba(255, 255, 255, 0.1)",
+    height: "75%",
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
     borderRadius: 10,
   },
   writeTaskWrapper: {
@@ -111,29 +118,29 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     alignItems: "center",
   },
-input: {
-  paddingVertical: 15,
-  paddingHorizontal: 15,
-  backgroundColor: "#FFF",
-  borderRadius: 60,
-  borderColor: "#C0C0C0",
-  borderWidth: 1,
-  width: 250,
-},
-addWrapper: {
-  width: 60,
-  height: 60,
-  backgroundColor: "#FFF",
-  borderRadius: 60,
-  justifyContent: "center",
-  alignItems: "center",
-  borderColor: "#C0C0C0",
-  borderWidth: 1,
-},
-background: {
-  flex: 1,
-  width: '100%',
-  height: '100%',
-},
-addText: {},
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  addText: {},
 });
